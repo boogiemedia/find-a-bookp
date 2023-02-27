f4266e039204698934fd1c413b7daa2bfb032af1
boogiemedia <speedysokol@gmail.com>
2023-02-28 01:28:55 +0200

    added api

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,100 +1,95 @@
-import React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import LoadingButton from '@mui/lab/LoadingButton';
+import React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import LoadingButton from "@mui/lab/LoadingButton";
 
-
-
-
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+interface props {
+  text: any;
+  handleApi: any;
+}
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '20ch',
-      '&:focus': {
-        width: '30ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "20ch",
+      "&:focus": {
+        width: "30ch",
       },
     },
   },
 }));
 
-export default function Header() {
+export default function Header({ text, handleApi }: props) {
   const [loading, setLoading] = React.useState(true);
-  const handleClick =()=>{
-    setLoading( false)
-  }
+  const handleClick = () => {
+    handleApi();
+    setLoading(false);
+  };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            Find a Book
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-
-/>
-<LoadingButton 
-          size="large"
-          onClick={handleClick}
-          loading={!loading}
-    
-          loadingPosition="end"
-          variant="contained"
+    <AppBar position="static">
+      <Toolbar>
+        <Typography
+          variant="h5"
+          noWrap
+          component="div"
+          sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
         >
-          <span>Find</span>
-        </LoadingButton >
-          </Search>
-          
-        </Toolbar>
-      </AppBar>
-    </Box>
+          Find a Book
+        </Typography>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            onChange={(e) => text(e.target.value)}
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+          <LoadingButton
+            size="large"
+            onClick={handleClick}
+            loading={!loading}
+            loadingPosition="end"
+            variant="contained"
+          >
+            <span>Find</span>
+          </LoadingButton>
+        </Search>
+      </Toolbar>
+    </AppBar>
   );
 }
--- src/components/main/toolBar.tsx
+++ src/components/main/toolBar.tsx
@@ -1,22 +1,73 @@
 import React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
 
 export default function ToolBar() {
+  const [category, setCategory] = React.useState("");
+  const handleChange = (event: { target: { value: string } }) => {
+    setCategory(event.target.value);
+  };
+  const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    "label + &": {
+      marginTop: theme.spacing(3),
+    },
+    "& .MuiInputBase-input": {
+      borderRadius: 4,
+      position: "relative",
+      backgroundColor: theme.palette.background.paper,
+      border: "1px solid #ced4da",
+      fontSize: 16,
+      padding: "10px 26px 10px 12px",
+    },
+  }));
   return (
-    <Box sx={{ flexGrow: 1,
-    dispay: 'none' }}>
-      <AppBar position="static">
-        <Toolbar
-          sx={{
-            backgroundColor: "white",
-          }}
-        ></Toolbar>
-      </AppBar>
-    </Box>
+    <Toolbar
+      sx={{
+        backgroundColor: "white",
+      }}
+    >
+      <Typography noWrap component="div">
+        From price
+      </Typography>
+      <FormControl sx={{ m: 1, width: "10ch" }} variant="outlined">
+        <OutlinedInput
+          id="outlined-adornment-price"
+          endAdornment={<InputAdornment position="end">$</InputAdornment>}
+          aria-describedby="outlined-weight-helper-text"
+        />
+      </FormControl>
+      <Typography noWrap component="div">
+        To price
+      </Typography>
+      <FormControl sx={{ m: 1, width: "10ch" }} variant="outlined">
+        <OutlinedInput
+          id="outlined-adornment-price"
+          endAdornment={<InputAdornment position="end">$</InputAdornment>}
+          aria-describedby="outlined-weight-helper-text"
+        />
+      </FormControl>
+      <Typography noWrap component="div" sx={{ marginLeft: "20px" }}>
+        Category
+      </Typography>
+      <FormControl sx={{ m: 1 }} variant="standard">
+        <NativeSelect
+          id="demo-customized-select-native"
+          value={category}
+          onChange={handleChange}
+          input={<BootstrapInput />}
+        >
+          <option aria-label="None" value="" />
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </NativeSelect>
+      </FormControl>
+    </Toolbar>
   );
 }
